b0ecaead217a52d278eb6415933fa270645c0fb4
Victor Huang <victorhuang@victors-macbook-pro.local>
2018-02-07 19:51:27 -0800

    Initial Commit

---
+++ Personality Makeup TestUITests/Personality_Makeup_TestUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Personality_Makeup_TestUITests.swift
+//  Personality Makeup TestUITests
+//
+//  Created by Victor Huang on 2/7/18.
+//  Copyright © 2018 Victor Huang. All rights reserved.
+//
+
+import XCTest
+
+class Personality_Makeup_TestUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
